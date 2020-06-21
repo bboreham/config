017ae8933b608fef76128256322650cc6a91c1ef
Bryan Boreham <bjboreham@gmail.com>
2020-06-21 16:32:52 +0000

    Better melpa for emacs

--- init.el
+++ init.el
@@ -1,7 +1,16 @@
 ;; Add Melpa location so we can install Magit.
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
 (require 'package)
 (add-to-list 'package-archives
-             '("melpa" . "http://melpa.org/packages/") t)
+   '("melpa" . "http://melpa.milkbox.net/packages/")
+;;   '("melpa" . "http://melpa.org/packages/")
+   t)
 
 (global-set-key [f8] (quote compile))
 (global-set-key [f5] (quote revert-buffer))
@@ -35,6 +44,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (go-mode)))
  '(whitespace-style (quote (trailing))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
